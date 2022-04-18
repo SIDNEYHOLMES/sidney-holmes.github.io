a52b6c455f6a0713a8a6257553a33565c9b42719
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2022-04-18 03:06:16 +0000

    day 4 of scratch-pad complete

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -35,7 +35,11 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-
+return {
+    id: id,
+    nameFirst: nameFirst,
+    nameLast: nameLast,
+}
 } 
 
 
@@ -43,16 +47,49 @@ function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts;
+    var contacts = [];
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
+        },
+        addContact: function(contact) {
+            return contacts.push(contact)
+        },/**takes a full-name String, like 'Max Gaudin', and 
+        *         returns the contact object if found in the contacts-list, or, 
+        *         undefined if the fullName does not match any contacts in the list. */
+        findContact: function(fullName) {
+            // this will hold the fullName value split into first and last name
+            var names = fullName.split(" ")
+            // if firstname or lastname match with the current iteration of contacts firstname or lastname return the object else return undefined
+            for (var i = 0; i < contacts.length; i++) {
+           if (names[0] === contacts[i].nameFirst && names[1] === contacts[i].nameLast) {
+               return contacts[i]
+           }
+        }  return undefined
+
+        },
+        removeContact: function(contact) {
+            //takes a contact object to be removed from the contacts list
+            for (var i = 0; i < contacts.length; i++) {
+                if (contacts[i].nameFirst === contact.nameFirst && contacts[i].nameLast === contact.nameLast) {
+                    contacts.splice(0, 1 )
+                }
+            }
+        },//this function should return a string formatted with all the fullnames of the seperated with a linebreak: ex 
+        printAllContactNames: function() {
+                var arr = []; // this array will store all of our names 
+                for (var i = 0; i < contacts.length; i++) { // iterate through contacts 
+                    var fullName = `${contacts[i].nameFirst} ${contacts[i].nameLast}` // take the first and last name and put it into an array
+                    arr.push(fullName) // push fullname into arr 
+                }
+                if (arr.length === contacts.length) { // if the array is full with the contacts fullnames return 
+                    return arr.join("\n")
+                }
         }
     }
 }
-
 
 
 
