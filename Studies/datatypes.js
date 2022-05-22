3b6e152c89f628f2e6ede923ec0cce8fde9686c6
sholm <sidneyselassie1@gmail.com>
2022-05-22 02:04:49 +0000

    studies save

--- Studies/datatypes.js
+++ Studies/datatypes.js
@@ -4,36 +4,47 @@
  * 
  * String
  * var str = "string"
- * strings are used to represent textual data 
+ * strings are used to represent textual data (i.e. sequences of characters).
  * 
- * Number
- * var num = 33
+ * Number       or
+ * var num = -33 || 3.33
+ * numbers are used to represent positive or negative numbers with or without decimals
  * 
- * Boolean
+ * Boolean         or
  * var bool = true || false
- * 
+ * a boolean is any two values of true or false. boolean values also come as a result of a comparison in a program
+ *
  * Object
  * var obj = {Name: "sid", Age: 17}
- * 
+ * objects are a complex data type that contains properies defined as key-value pair. The property of key (name, age) is always a string while
+ * the value property can be any data type (string, number, boolean, array, function, object)
+ *
  * Array
- * var arr = ["str", num, bool, 33 ]
- * 
+ * var arr = ["str", 3.4, false, 33 ]
+ *      index:  0     1     2     3
+ * arrays are  a type of object used for storing multiple values in a single variable. each value in an array has a index position
+ * an index always starts at 0 meaning that "str" in our example array is at the 0 index 
+ *
  * Function
  * var func = function () {//do cool thing that works with no bugs}
- * 
+ * functions are a callable object that executes a block of code. since functions are a type of object it is possible to assign them to a variable 
+ *
  * Undefined
  * var und; // this would equal undefined because there is no initilization with this variable
- * 
+ * undefined is a data type that can only have one value-the special undefined value.
+ *
  * Null
  * var nul = null 
+ * this is also a special data type that can only have one value-the special null value.
  *
  * NaN
  * var not_number = NaN
+ * NaN stands for not a number and is another special data type that can be interpreted as a value that is undefined or null
  *
- *infinity
+ * infinity
  *
  *
- *
+ *Coby by value/ copy by reference
  *
  *
  */
